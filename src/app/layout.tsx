61a90e7f1ce804ea9629cbff3580a836d34a806d
Mikael <s.h.koo1213@gmail.com>
2023-10-18 19:51:54 -0400

    Issue 4: configure prettier - script and pre-commit hook

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,27 +1,26 @@
-import './globals.css'
-import type { Metadata } from 'next'
-import { Inter } from 'next/font/google'
-import Footer from './components/Footer/Footer'
+import './globals.css';
+import type { Metadata } from 'next';
+import { Inter } from 'next/font/google';
+import Footer from './components/Footer/Footer';
 
-const inter = Inter({ subsets: ['latin'] })
+const inter = Inter({ subsets: ['latin'] });
 
 export const metadata: Metadata = {
-  title: 'DeckRocket',
-  description: 'E-commerce website for cards',
-}
+	title: 'DeckRocket',
+	description: 'E-commerce website for cards'
+};
 
 export default function RootLayout({
-  children,
+	children
 }: {
-  children: React.ReactNode
+	children: React.ReactNode;
 }) {
-  return (
-    <html lang="en" className={inter.className}>
-      <body>
-
-        {children}
-        <Footer />
-      </body>
-    </html>
-  )
+	return (
+		<html lang="en" className={inter.className}>
+			<body>
+				{children}
+				<Footer />
+			</body>
+		</html>
+	);
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,33 +1,33 @@
-import Link from "next/link";
-import SearchResultCard from "./components/SearchResultCard";
-import ShoppingCartCard from "./components/ShoppingCartCard";
+import Link from 'next/link';
+import SearchResultCard from './components/SearchResultCard';
+import ShoppingCartCard from './components/ShoppingCartCard';
 
 export default function Home() {
-  return (
-    <>
-      <h1>This is Main Page!</h1>
-      <Link href="/testpage">Go to Test Page</Link>
+	return (
+		<>
+			<h1>This is Main Page!</h1>
+			<Link href="/testpage">Go to Test Page</Link>
 
-      <SearchResultCard
-        game="Magic: The Gathering"
-        setType="Commander Masters"
-        name="Kodama's Reach (Borderless)"
-        imgUrl="/assets/images/magic-kodamas-reach.png"
-        rarity="Common"
-        id={649}
-        price={0.39}
-        currency="CAD"
-      />
+			<SearchResultCard
+				game="Magic: The Gathering"
+				setType="Commander Masters"
+				name="Kodama's Reach (Borderless)"
+				imgUrl="/assets/images/magic-kodamas-reach.png"
+				rarity="Common"
+				id={649}
+				price={0.39}
+				currency="CAD"
+			/>
 
-      <ShoppingCartCard 
-        setType="Commander Masters"
-        name="Kodama's Reach (Borderless)"
-        imgUrl="/assets/images/magic-kodamas-reach.png"
-        quality="Lightly Played"
-        foilType="Non Foil"
-        price={0.39}
-        currency="CAD"
-      /> 
-    </>
-  );
+			<ShoppingCartCard
+				setType="Commander Masters"
+				name="Kodama's Reach (Borderless)"
+				imgUrl="/assets/images/magic-kodamas-reach.png"
+				quality="Lightly Played"
+				foilType="Non Foil"
+				price={0.39}
+				currency="CAD"
+			/>
+		</>
+	);
 }
